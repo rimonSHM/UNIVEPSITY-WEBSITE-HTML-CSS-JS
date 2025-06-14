c87dffcf046593b0cc190afa5ba0665b8f78d5f5
Md: Rimon Hasan <rimon072159@gmail.com>
2025-06-14 01:05:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+ $('#menu-close').click(function(){
+     $('nav .navigation').removeClass('active')
+ });
+ 
+ $('#menu-btn').click(function(){
+     $('nav .navigation').addClass('active')
+ });
+
+  
